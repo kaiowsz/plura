3a3047cda124fc6503635f9fdfe66217bdcd3232
kaiowsz <kaionpsa@gmail.com>
2025-05-10 10:43:56 -0300

    feat: Implement agencyId page

---
+++ src/app/(main)/agency/[agencyId]/page.tsx
@@ -0,0 +1,17 @@
+import AgencyDetails from "@/components/forms/AgencyDetails";
+import React from "react"
+
+type Props = {
+    params: {
+        agencyId: string;
+    }
+}
+
+const AgencyIDPage = ({params: {agencyId}}: Props) => {
+  return (
+    <div className="relative h-full">
+    </div>
+  )
+}
+
+export default AgencyIDPage
