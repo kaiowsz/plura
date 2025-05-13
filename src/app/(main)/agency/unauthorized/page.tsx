9b65305340b2d5dbc56d0380fcdd400a18f1f905
kaiowsz <kaionpsa@gmail.com>
2025-05-13 10:34:26 -0300

    feat: Implement pages from agency path

---
+++ src/app/(main)/agency/unauthorized/page.tsx
@@ -0,0 +1,12 @@
+import Unauthorized from '@/components/global/Unauthorized'
+import React from 'react'
+
+type Props = {}
+
+const NotAuthorized = (props: Props) => {
+    return (
+    <Unauthorized />
+    )
+}
+
+export default NotAuthorized
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import ModalProvider from "@/providers/ModalProvider";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Plura",
