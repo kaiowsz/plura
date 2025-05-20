eec166ecaffde670cb4b7a1b0179fec612007fd2
kaiowsz <kaionpsa@gmail.com>
2025-05-20 10:27:50 -0300

    chore: Add types & utils

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,7 @@
-import { Notification, Prisma, Role } from "@prisma/client";
-import { getAuthUserDetails, getUserPermissions } from "./queries";
+import { Contact, Lane, Notification, Prisma, Role, Tag, Ticket, User } from "@prisma/client";
+import { _getTicketsWithAllRelations, getAuthUserDetails, getMedia, getPipelineDetails, getTicketsWithTags, getUserPermissions } from "./queries";
+import { db } from "./db";
+import { z } from "zod";
 
 export type NotificationWithUser =
   | ({
@@ -19,3 +21,63 @@ export type NotificationWithUser =
 export type UserWithPermissionsAndSubAccounts = Prisma.PromiseReturnType<typeof getUserPermissions>;
 
 export type AuthUserWithAgencySidebarOptionsSubAccounts = Prisma.PromiseReturnType<typeof getAuthUserDetails>;
+
+const __getUsersWithAgencySubAccontPermissionsSidebarOptions = async (agencyId: string) => {
+  return await db.user.findFirst({
+    where: {
+      Agency: {
+        id: agencyId,
+      },
+    },
+    include: {
+      Agency: {
+        include: {
+          SubAccount: true,
+        }
+      },
+      Permissions: {
+        include: {
+          SubAccount: true,
+        }
+      }
+    }
+  })
+}
+
+export type UsersWithAgencySubAccountPermissionsSidebarOptions = Prisma.PromiseReturnType<typeof __getUsersWithAgencySubAccontPermissionsSidebarOptions>;
+
+export type GetMediaFiles = Prisma.PromiseReturnType<typeof getMedia>;
+
+export type CreateMediaType = Prisma.MediaCreateWithoutSubaccountInput;
+
+export type TicketAndTags = Ticket & {
+  Tags: Tag[],
+  Assigned: User | null;
+  Customer: Contact | null;
+}
+
+export type LaneDetail = Lane & {
+  Tickets: TicketAndTags[]
+}
+
+export const CreateFunnelFormSchema = z.object({
+  name: z.string().min(1),
+  description: z.string(),
+  subDomainName: z.string().optional(),
+  favicon: z.string().optional(),
+})
+
+export type PipelineDetailsWithCardsTagsTickets = Prisma.PromiseReturnType<typeof getPipelineDetails>;
+
+export const LaneFormSchema = z.object({
+  name: z.string().min(1),
+})
+
+export type TicketWithTags = Prisma.PromiseReturnType<typeof getTicketsWithTags>
+
+export type TicketDetails = Prisma.PromiseReturnType<typeof _getTicketsWithAllRelations>;
+
+export const ContactUserFormSchema = z.object({
+  name: z.string().min(1, "Required"),
+  email: z.string().email()
+})
