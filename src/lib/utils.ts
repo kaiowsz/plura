9f1f86462a6deee075945d6e83e7808bb52c3bc9
kaiowsz <kaionpsa@gmail.com>
2025-06-04 08:49:30 -0300

    chore: Update utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,21 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 export const currencyNumberRegex = /^\d+(\.\d{1,2})?$/
+
+// export const getStripeOAuthLink = (
+//   accountType: "agency" | "subaccount",
+//   state: string
+// ) => {
+//   return `https://connect.stripe.com/oauth/v2/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_STRIPE_CLIENT_ID_TEST}&scope=read_write&redirect_uri=${process.env.NEXT_PUBLIC_URL}${accountType}&state=${state}`;
+// };
+
+export function getStripeOAuthLink(
+  accountType: "agency" | "subaccount",
+  state: string
+) {
+  // Reference URL from the Stripe Connect OAuth Docs
+
+  // https://connect.stripe.com/oauth/authorize?response_type=code&client_id={{CLIENT_ID}}&scope=read_write&redirect_uri=https://sub2.example.com
+
+  return `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_STRIPE_CLIENT_ID_TEST}&scope=read_write&redirect_uri=${process.env.NEXT_PUBLIC_URL}${accountType}&state=${state}`;
+}
